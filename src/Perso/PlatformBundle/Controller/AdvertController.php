822906c8adf9c58accd84893c664aca27f9bebd0
Ertan <e.ozaydin@groupe-reussite.fr>
2017-01-20 14:09:50 +0100

    Commit Mid day

--- src/Perso/PlatformBundle/Controller/AdvertController.php
+++ src/Perso/PlatformBundle/Controller/AdvertController.php
@@ -18,22 +18,42 @@ class AdvertController extends Controller
         // $content = $this->get('templating')->render('PersoPlatformBundle:Advert:index.html.twig', $tableau = array('nom' => 'ertoun'));
 
         // return new Response($content);
-        if ($page < 1) {
-      // On déclenche une exception NotFoundHttpException, cela va afficher
-      // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
-      throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+        if ($page = 0) {throw new NotFoundHttpException('Page "'.$page.'" inexistante.');}
+        else {
+                $listAdverts = array(
+                    array(
+                        'title'   => 'Recherche développpeur Symfony',
+                        'id'      => 1,
+                        'author'  => 'Alexandre',
+                        'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                        'date'    => new \Datetime()),
+                      array(
+                        'title'   => 'Mission de webmaster',
+                        'id'      => 2,
+                        'author'  => 'Hugo',
+                        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                        'date'    => new \Datetime()),
+                      array(
+                        'title'   => 'Offre de stage webdesigner',
+                        'id'      => 3,
+                        'author'  => 'Mathieu',
+                        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                        'date'    => new \Datetime())
+                      );
+                return $this->render('PersoPlatformBundle:Advert:index.html.twig', array('listAdverts' => $listAdverts));
+            }
     }
-
-    // Ici, on récupérera la liste des annonces, puis on la passera au template
-
-    // Mais pour l'instant, on ne fait qu'appeler le template
-    return $this->render('PersoPlatformBundle:Advert:index.html.twig');
-    }
-
     public function viewAction($id, Request $request)
     {
+        $advert = array(
+        'title'   => 'Recherche développpeur Symfony2',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime());
+
         return $this->render('PersoPlatformBundle:Advert:view.html.twig', array(
-      'id' => $id
+      'advert' => $advert
     ));
     }
 
@@ -68,16 +88,17 @@ class AdvertController extends Controller
 
   public function editAction($id, Request $request)
   {
-    // Ici, on récupérera l'annonce correspondante à $id
+    $advert = array(
+      'title'   => 'Recherche développpeur Symfony',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Lemon',
+      'date'    => new \Datetime()
+    );
 
-    // Même mécanisme que pour l'ajout
-    if ($request->isMethod('POST')) {
-      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
-
-      return $this->redirectToRoute('Perso_platform_view', array('id' => 5));
-    }
-
-    return $this->render('PersoPlatformBundle:Advert:edit.html.twig');
+    return $this->render('PersoPlatformBundle:Advert:edit.html.twig', array(
+      'advert' => $advert
+    ));
   }
 
   public function deleteAction($id)
@@ -88,4 +109,19 @@ class AdvertController extends Controller
 
     return $this->render('PersoPlatformBundle:Advert:delete.html.twig');
   }
+
+  public function menuAction()
+  {
+        $listAdverts = array(
+      array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+      array('id' => 5, 'title' => 'Mission de webmaster'),
+      array('id' => 9, 'title' => 'Offre de stage webdesigner')
+    );
+
+    return $this->render('PersoPlatformBundle:Advert:menu.html.twig', array(
+      // Tout l'intérêt est ici : le contrôleur passe
+      // les variables nécessaires au template !
+      'listAdverts' => $listAdverts
+    ));
+    }
 }
